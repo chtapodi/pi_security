640238d74387cf25c896f65aba13108d2a8d372b
xavier <x.t.quinn@gmail.com>
2019-10-20 02:10:56 -0400

    first commit, everything already works. Autoimprove auto improves

--- security.py
+++ security.py
@@ -9,6 +9,7 @@ import scipy.misc
 import telegram_send as ts
 import imageio
 import shutil
+import time
 
 from datetime import datetime
 now = datetime.now() # current date and time
@@ -19,14 +20,17 @@ def fprint(text, value) :
 
 
 def approved_handler(identity_list) :
-	print("this is {0}".format(identity_list))
-	telegram_send_image("Welcome {}".format(', '.join(identity_list)).title())
+	name_list=set(identity_list)
+	print("this is {0}".format(name_list))
+	telegram_send_image("Welcome {}".format(', '.join(name_list)).title())
+
+	improve_recognition(identity_list)
 
 #captures new IMG, returns it
 def capture(camera) :
 	output = np.empty((240, 320, 3), dtype=np.uint8)
 	camera.capture(output, format="rgb")
-	print("writeout")
+
 	imageio.imwrite('tmp.jpg', output) #saves a local file which can be messed with
 	return output
 
@@ -36,7 +40,7 @@ def classify_and_handle(captured_enconding, approved_collection, rejected_collec
 	rejected_list=[]
 	rejected_list=face_compare(captured_enconding, rejected_collection) #see who is approved
 
-	if (len(captured_enconding)>0) : #bc all the faces may be rejected
+	if (len(captured_enconding)>(len(rejected_list))) : #bc all the faces may be rejected
 		approved_list=face_compare(captured_enconding, approved_collection) #see who is approved
 
 	if (len(approved_list)>0) :
@@ -45,11 +49,12 @@ def classify_and_handle(captured_enconding, approved_collection, rejected_collec
 	if (len(rejected_list)>0) :
 		rejected_handler(rejected_list)
 
-	if (len(captured_enconding)>0) : #if the removal of elements in the captured_enconding list has worked, this will be 0 only if there are unknown indeviduals
+	if (len(captured_enconding)>(len(rejected_list)+len(approved_list))) : #if the removal of elements in the captured_enconding list has worked, this will be 0 only if there are unknown indeviduals
 		unknown_handler()
 
 def count_files_in_dir(dir) :
-	return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+	return len(get_dir_list(dir))
+
 
 #captures and extracts faces and their locations, returning them
 def extract_faces(camera):
@@ -61,6 +66,8 @@ def extract_faces(camera):
 		return encoding_list
 	return []
 
+def get_dir_list(dir):
+	return [name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))]
 
 #compares faces, returns values
 def face_compare(captured_enconding, collection) :
@@ -85,8 +92,23 @@ def get_file_paths(path) :
 			file_list.append(os.path.join(path, filename))
 	return file_list
 
+
+def improve_recognition(identity_list) :
+	if (len(set(identity_list))<=1) : #if theres only one face
+		approved_time=time.time()
+		unknown_list=get_dir_list("unknown/")
+		time_list=[int(name.split(".")[0]) for name in unknown_list]
+		diff_list=[int(approved_time-time) for time in time_list]
+		for i in range(len(diff_list)) :
+			if (diff_list[i]<5) :#if theres images closer than 5 seconds
+				print("Attempting to improve recognition rates")
+				to_move=unknown_list[i]
+				print("to_move {}".format(to_move))
+				shutil.move("unknown/{}".format(to_move), 'approved_faces/{0}.{1}.jpg'.format(identity_list[0],time_list[i]))
+
+
 def move_to_unknown() :
-	shutil.move("tmp.jpg", "unknown/{0}.jpg".format(now.strftime("%m_%d_%Y%_H_%M_%S")))
+	shutil.move("tmp.jpg", "unknown/{0}.jpg".format(int(time.time())))
 
 #takes in an input of img paths and returns a list of encoded img values
 def load_encoding(path_list) :
@@ -104,11 +126,14 @@ def load_faces(path) :
 	encoding_list=load_encoding(path_list)
 	return [encoding_list, name_list]
 
+
+
 #takes in a list of paths and outputs a list of just the names of the files
 def path_to_name(path_list) :
 	name_list=[]
 	for file in path_list :
 		name=os.path.splitext(os.path.basename(file))[0]
+		name=name.split(".")[0]
 		name_list.append(name)
 	return name_list
 
@@ -123,6 +148,7 @@ def telegram_send_image(message) :
 def unknown_handler() :
 	telegram_send_image("Unknown")
 	move_to_unknown()
+	print("unkown")
 
 
 def main():
@@ -167,10 +193,12 @@ def main():
 		classify_and_handle(encoded_faces, approved_collection,rejected_collection)
 
 		if(count_files_in_dir(approved_path)>start_approved_length) :
+			print("Detected new approved data")
 			approved_collection=load_faces(approved_path)
 			start_approved_length=len(approved_collection[0])
 
 		if(count_files_in_dir(rejected_path)>start_rejected_length) :
+			print("Detected new reject")
 			rejected_collection=load_faces(rejected_path)
 			start_rejected_length=len(rejected_collection[0])
 
